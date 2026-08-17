224c8174a6d77ead665ecb14a9b7f99bf7c6fe39
Pranaam19 <126946924+Pranaam19@users.noreply.github.com>
2026-08-17 11:14:54 +0000

    ssl code

---
+++ SLL/slist.h
@@ -0,0 +1,32 @@
+#ifndef SLIST_H_INCLUDED
+#define SLIST_H_INCLUDED
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct __node__ Node;
+typedef struct __slist__ List;
+struct __slist__{
+    Node *head;
+    Node *tail;
+    int32_t length;
+    };
+struct __node__{
+    int32_t data;
+    Node *next;
+};
+
+List * slist_new();
+List * slist_free(List*);
+uint32_t slist_length(const List * list);
+uint32_t slist_lookup(const List * list,int32_t key);
+List * slist_add_head(List * list,int32_t data);
+List * slist_add_tail(List * list,int32_t data);
+List * slist_delete_head(List * list);
+List * slist_delete_tail(List * list);
+
+
+
+
+
+#endif // SLIST_H_INCLUDED
